5927a345059cad814b4227625cc2ed5041eed673
fitzgebt <fitzgebt@gmail.com>
2021-06-01 12:13:26 -0700

    set up basic frontend file structure.

--- frontend/rant/src/App.js
+++ frontend/rant/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React, { Component }  from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component  {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row title justify-content-center" style={{ paddingTop: '12px' }}>
+          <h1>Test App Maker</h1>
+        </div>
+        <hr />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ frontend/rant/src/reducers/posts.js
@@ -0,0 +1,16 @@
+export default (state = [], action) => {
+
+    let index;
+    let post;
+
+    switch (action.type) {
+        
+        case 'ADD_POST':
+            return state.concat(action.post);
+        
+        default: 
+            return state;
+    }
+
+
+}
