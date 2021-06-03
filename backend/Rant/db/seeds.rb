3d4db47104b8dd373a4c9d0f1daee6c96445a3bc
fitzgebt <fitzgebt@gmail.com>
2021-06-03 11:23:41 -0700

    created basic seed file to test start fresh post loading from db.

--- backend/Rant/db/seeds.rb
+++ backend/Rant/db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p1 = Post.new
+p1.text = "My job sent me a cup and a pack of mints to thank me for my 5 years…I’m on Indeed as we speak"
+p1.author = "Coach C."
+p1.save
+
+p2 = Post.new
+p2.text = "A guy got mad and told to take my mask off at a gas station. I’ve practiced this interaction in my head a million times. but I got nervous and yelled “fat chance!!!”....like some sort of 40s newsboy"
+p2.author = "Laura Peek"
+p2.save
+
+p3 = Post.new
+p3.text = "My apartment is overdue for a deep cleaning, if anyone wants to give me a writing deadline"
+p3.author = "Brandon Follick"
+p3.save
